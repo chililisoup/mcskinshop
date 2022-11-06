25428b3dcafcf4d2bf2cf396bcefe00d5a17daa7
rebot333 <47825189+rebot333@users.noreply.github.com>
2022-11-06 03:01:41 -0700

    filters + some stuff
    Layer filters
    Legacy skin support
    Hat Flattener rewrite
    Reverse layer list order

--- src/imgmod.js
+++ src/imgmod.js
@@ -1,75 +1,21 @@
 const hatFlattenerOffsets = [
-    {
-        width: 16,
-        height: 8,
-        from: [40, 0],
-        to: [8, 0]
-    },
-    {
-        width: 32,
-        height: 8,
-        from: [32, 8],
-        to: [0, 8]
-    },
-    {
-        width: 8,
-        height: 4,
-        from: [4, 32],
-        to: [4, 16]
-    },
-    {
-        width: 16,
-        height: 4,
-        from: [20, 32],
-        to: [20, 16]
-    },
-    {
-        width: 8,
-        height: 4,
-        from: [44, 32],
-        to: [44, 16]
-    },
-    {
-        width: 56,
-        height: 12,
-        from: [0, 36],
-        to: [0, 20]
-    },
-    {
-        width: 8,
-        height: 4,
-        from: [4, 48],
-        to: [20, 48]
-    },
-    {
-        width: 16,
-        height: 12,
-        from: [0, 52],
-        to: [16, 52]
-    },
-    {
-        width: 8,
-        height: 4,
-        from: [52, 48],
-        to: [36, 48]
-    },
-    {
-        width: 16,
-        height: 12,
-        from: [48, 52],
-        to: [32, 52]
-    }
+    { width: 16, height: 8,  from: [40, 0 ], to: [8,  0 ] },
+    { width: 32, height: 8,  from: [32, 8 ], to: [0,  8 ] },
+    { width: 8,  height: 4,  from: [4,  32], to: [4,  16] },
+    { width: 16, height: 4,  from: [20, 32], to: [20, 16] },
+    { width: 8,  height: 4,  from: [44, 32], to: [44, 16] },
+    { width: 56, height: 12, from: [0,  36], to: [0,  20] },
+    { width: 8,  height: 4,  from: [4,  48], to: [20, 48] },
+    { width: 16, height: 12, from: [0,  52], to: [16, 52] },
+    { width: 8,  height: 4,  from: [52, 48], to: [36, 48] },
+    { width: 16, height: 12, from: [48, 52], to: [32, 52] }
 ];
 
-function coordsToImageDataIndices(x, y) {
-    const i = 4 * (y * 64 + x);
-    return [i, i + 1, i + 2, i + 3];
-}
-
 export class Img {
-    constructor(type, blend) {
+    constructor(type, blend, filter) {
         this.type = type || "normal";
         this.blend = blend || "source-over";
+        this.filter = filter || "";
     }
 
     render = url => new Promise((resolve, reject) => {
@@ -87,10 +33,29 @@ export class Img {
                 const ctx = canvas.getContext("2d");
                 ctx.drawImage(result, 0, 0);
 
-                this.src = canvas.toDataURL();
-                this.image = result;
+                if (image.height === 32) {
+                    ctx.setTransform(-1, 0, 0, 1, 64, 0); // Mirror, move into frame
 
-                resolve();
+                    ctx.drawImage(result, 44, 16, 4,  4,  24, 48, 4,  4 ); // Arm top
+                    ctx.drawImage(result, 48, 16, 4,  4,  20, 48, 4,  4 ); // Arm bottom
+                    ctx.drawImage(result, 40, 20, 12, 12, 20, 52, 12, 12); // Arm front/sides
+                    ctx.drawImage(result, 52, 20, 4,  12, 16, 52, 4,  12); // Arm back
+
+                    ctx.drawImage(result, 4,  16, 4,  4,  40, 48, 4,  4 ); // Leg top
+                    ctx.drawImage(result, 8,  16, 4,  4,  36, 48, 4,  4 ); // Leg bottom
+                    ctx.drawImage(result, 0,  20, 12, 12, 36, 52, 12, 12); // Leg front/sides
+                    ctx.drawImage(result, 12, 20, 4,  12, 32, 52, 4,  12); // Leg back
+
+                    this.src = canvas.toDataURL();
+                    createImageBitmap(canvas).then(imageResult => {
+                        this.image = imageResult;
+                        resolve();
+                    });
+                } else {
+                    this.src = canvas.toDataURL();
+                    this.image = result;
+                    resolve();
+                }
             });
         }
         
@@ -137,6 +102,7 @@ export class Img {
     }
 
     propagateBlendMode = blend => this.blend = blend || this.blend || "source-over";
+    propagateFilter = filter => this.filter = filter || this.filter || "";
 
     detectSlimModel = () => {
         if (!this.image) return;
@@ -151,41 +117,37 @@ export class Img {
     }
 
     flattenWithRespect = ctx => {
-        let imageData = ctx.getImageData(0, 0, 64, 64);
-        const flatMap = this.getImageData();
-        if (!flatMap) return;
+        const canvas = document.createElement("canvas");
+        canvas.width = 64;
+        canvas.height = 64;
+        const context = canvas.getContext("2d");
 
-        hatFlattenerOffsets.forEach(offset => {
-            for (let i = 0; i < offset.height * offset.width; i++) {
-                const fromX = i % offset.width + offset.from[0];
-                const fromY = Math.floor(i / offset.width) + offset.from[1];
-                const fromPixel = coordsToImageDataIndices(fromX, fromY);
-                if (!flatMap.data[fromPixel[3]]) continue;
+        context.drawImage(ctx.canvas, 0, 0);
+        context.globalCompositeOperation = "destination-in";
+        context.drawImage(this.image, 0, 0);
 
-                const toX = fromX - offset.from[0] + offset.to[0];
-                const toY = fromY - offset.from[1] + offset.to[1];
-                const toPixel = coordsToImageDataIndices(toX, toY);
-
-                const fromAlpha = imageData.data[fromPixel[3]] / 255;
-
-                imageData.data[toPixel[0]] = Math.floor(imageData.data[fromPixel[0]] * fromAlpha + imageData.data[toPixel[0]] * (1 - fromAlpha));
-                imageData.data[toPixel[1]] = Math.floor(imageData.data[fromPixel[1]] * fromAlpha + imageData.data[toPixel[1]] * (1 - fromAlpha));
-                imageData.data[toPixel[2]] = Math.floor(imageData.data[fromPixel[2]] * fromAlpha + imageData.data[toPixel[2]] * (1 - fromAlpha));
-                imageData.data[toPixel[3]] = Math.min(imageData.data[fromPixel[3]] + imageData.data[toPixel[3]], 255);
-                
-            }
-        });
-        ctx.putImageData(imageData, 0, 0);
+        hatFlattenerOffsets.forEach(offset => ctx.drawImage(
+            canvas,
+            offset.from[0],
+            offset.from[1],
+            offset.width,
+            offset.height,
+            offset.to[0],
+            offset.to[1],
+            offset.width,
+            offset.height
+        ));
     }
 }
 
 
 
 export class Layer {
-    constructor(sublayers, colors, blend) {
+    constructor(sublayers, colors, blend, filter) {
         this.sublayers = sublayers || [];
         this.colors = colors || "#FFFFFF";
         this.blend = blend || "source-over";
+        this.filter = filter || "";
     }
 
     color = colors => new Promise((resolve, reject) => {
@@ -202,10 +164,17 @@ export class Layer {
         )).then(resolve);
     })
 
-    propagateBlendMode = (blend) => {
+    propagateBlendMode = blend => {
         this.blend = blend || this.blend || "source-over";
         this.sublayers.forEach(sublayer => {
             sublayer.propagateBlendMode(this.blend);
+        });
+    }
+
+    propagateFilter = filter => {
+        this.filter = filter || this.filter || "";
+        this.sublayers.forEach(sublayer => {
+            sublayer.propagateFilter(this.filter);
         });
     }
 
@@ -218,6 +187,7 @@ export class Layer {
             ctx = canvas.getContext("2d");
         }
         this.sublayers.forEach(sublayer => {
+            ctx.filter = "opacity(100%) hue-rotate(0) saturate(100%) brightness(100%) contrast(100%) invert(0) sepia(0)";
             if (sublayer instanceof Layer)
                 sublayer.render(ctx);
             else {
@@ -226,12 +196,12 @@ export class Layer {
                     ctx.globalCompositeOperation = "destination-out";
                     ctx.drawImage(sublayer.image, 0, 0);
                 } else {
-
                     ctx.globalCompositeOperation = sublayer.blend;
+                    ctx.filter = sublayer.filter;
 
-                    if (sublayer.type === "erase") {
+                    if (sublayer.type === "erase")
                         ctx.globalCompositeOperation = "destination-out";
-                    }
+
                     ctx.drawImage(sublayer.image, 0, 0);
                 }
             }
--- src/layermanager.js
+++ src/layermanager.js
@@ -35,6 +35,15 @@ class Layer extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            opacity: 100,
+            hue: 0,
+            saturation: 100,
+            brightness: 100,
+            contrast: 100,
+            invert: 0,
+            sepia: 0
+        };
         this.asset = props.asset;
     }
 
@@ -56,8 +65,24 @@ class Layer extends Component {
         //ehh idk, this and a few other things would be fixed with a custom color picker
     }
 
-    changeBlendMode = (blend) => {
+    changeBlendMode = blend => {
         this.asset.propagateBlendMode(blend.target.value);
+        this.updateLayer();
+    }
+
+    updateFilter = filter => {
+        this.setState(filter);
+
+        filter =
+            "opacity(" + this.state.opacity + "%) " +
+            "hue-rotate(" + this.state.hue + "deg) " +
+            "saturate(" + this.state.saturation + "%) " +
+            "brightness(" + this.state.brightness + "%) " +
+            "contrast(" + this.state.contrast + "%) " +
+            "invert(" + this.state.invert + "%) " +
+            "sepia(" + this.state.sepia + "%)";
+
+        this.asset.propagateFilter(filter);
         this.updateLayer();
     }
 
@@ -85,8 +110,8 @@ class Layer extends Component {
                 <span>
                     <LayerPreview asset={this.asset} />
                     <div className="manager-layer-buttons">
-                        <button onClick={this.moveLayer.bind(this, -1)}>/\</button>
-                        <button onClick={this.moveLayer.bind(this, 1)}>\/</button>
+                        <button onClick={this.moveLayer.bind(this, 1)}>/\</button>
+                        <button onClick={this.moveLayer.bind(this, -1)}>\/</button>
                         <button onClick={this.removeLayer.bind(this)}>X</button>
                     </div>
                     <div className="manager-layer-colors">
@@ -94,37 +119,67 @@ class Layer extends Component {
                     </div>
                 </span>
                 {this.props.advanced &&
-                    <span>
-                        <label htmlFor={"blendSelector" + this.asset.id}>Blend Mode:</label>
-                        <select id={"blendSelector" + this.asset.id} value={this.asset.blend} onChange={this.changeBlendMode.bind(this)}>
-                            <option value="source-over">Source Over</option>
-                            <option value="source-in">Source In</option>
-                            <option value="source-out">Source Out</option>
-                            <option value="source-atop">Source Atop</option>
-                            <option value="destination-over">Destination Over</option>
-                            <option value="destination-in">Destination In</option>
-                            <option value="destination-out">Destination Out</option>
-                            <option value="destination-atop">Destination Atop</option>
-                            <option value="lighter">Lighter</option>
-                            <option value="copy">Copy</option>
-                            <option value="xor">XOR</option>
-                            <option value="multiply">Multiply</option>
-                            <option value="screen">Screen</option>
-                            <option value="overlay">Overlay</option>
-                            <option value="darken">Darken</option>
-                            <option value="lighten">Lighten</option>
-                            <option value="color-dodge">Color Dodge</option>
-                            <option value="color-burn">Color Burn</option>
-                            <option value="hard-light">Hard Light</option>
-                            <option value="soft-light">Soft Light</option>
-                            <option value="difference">Difference</option>
-                            <option value="exclusion">Exclusion</option>
-                            <option value="hue">Hue</option>
-                            <option value="saturation">Saturation</option>
-                            <option value="color">Color</option>
-                            <option value="luminosity">Luminosity</option>
-                        </select>
-                    </span>
+                    <div className="settingsList">
+                        <span>
+                            <label htmlFor={"blendSelector" + this.asset.id}>Blend Mode:</label>
+                            <select id={"blendSelector" + this.asset.id} value={this.asset.blend} onChange={this.changeBlendMode.bind(this)}>
+                                <option value="source-over">Source Over</option>
+                                <option value="source-in">Source In</option>
+                                <option value="source-out">Source Out</option>
+                                <option value="source-atop">Source Atop</option>
+                                <option value="destination-over">Destination Over</option>
+                                <option value="destination-in">Destination In</option>
+                                <option value="destination-out">Destination Out</option>
+                                <option value="destination-atop">Destination Atop</option>
+                                <option value="lighter">Lighter</option>
+                                <option value="copy">Copy</option>
+                                <option value="xor">XOR</option>
+                                <option value="multiply">Multiply</option>
+                                <option value="screen">Screen</option>
+                                <option value="overlay">Overlay</option>
+                                <option value="darken">Darken</option>
+                                <option value="lighten">Lighten</option>
+                                <option value="color-dodge">Color Dodge</option>
+                                <option value="color-burn">Color Burn</option>
+                                <option value="hard-light">Hard Light</option>
+                                <option value="soft-light">Soft Light</option>
+                                <option value="difference">Difference</option>
+                                <option value="exclusion">Exclusion</option>
+                                <option value="hue">Hue</option>
+                                <option value="saturation">Saturation</option>
+                                <option value="color">Color</option>
+                                <option value="luminosity">Luminosity</option>
+                            </select>
+                        </span>
+                        <span>
+                            <label htmlFor={"opacitySlider" + this.asset.id}>Opacity: {this.state.opacity}%</label>
+                            <input type="range" id={"opacitySlider" + this.asset.id} min={0} max={100} value={this.state.opacity} onChange={e => this.updateFilter({opacity: e.target.value})}/>
+                        </span>
+                        <span>
+                            <label htmlFor={"hueSlider" + this.asset.id}>Hue: {this.state.hue}°</label>
+                            <input type="range" id={"hueSlider" + this.asset.id} min={-180} max={180} value={this.state.hue} onChange={e => this.updateFilter({hue: e.target.value})}/>
+                        </span>
+                        <span>
+                            <label htmlFor={"saturationSlider" + this.asset.id}>Saturation: {this.state.saturation}%</label>
+                            <input type="range" id={"saturationSlider" + this.asset.id} min={0} max={200} value={this.state.saturation} onChange={e => this.updateFilter({saturation: e.target.value})}/>
+                        </span>
+                        <span>
+                            <label htmlFor={"brightnessSlider" + this.asset.id}>Brightness: {this.state.brightness}%</label>
+                            <input type="range" id={"brightnessSlider" + this.asset.id} min={0} max={200} value={this.state.brightness} onChange={e => this.updateFilter({brightness: e.target.value})}/>
+                        </span>
+                        <span>
+                            <label htmlFor={"contrastSlider" + this.asset.id}>Contrast: {this.state.contrast}%</label>
+                            <input type="range" id={"contrastSlider" + this.asset.id} min={0} max={200} value={this.state.contrast} onChange={e => this.updateFilter({contrast: e.target.value})}/>
+                        </span>
+                        <span>
+                            <label htmlFor={"invertSlider" + this.asset.id}>Invert: {this.state.invert}%</label>
+                            <input type="range" id={"invertSlider" + this.asset.id} min={0} max={100} value={this.state.invert} onChange={e => this.updateFilter({invert: e.target.value})}/>
+                        </span>
+                        <span>
+                            <label htmlFor={"sepiaSlider" + this.asset.id}>Sepia: {this.state.sepia}%</label>
+                            <input type="range" id={"sepiaSlider" + this.asset.id} min={0} max={100} value={this.state.sepia} onChange={e => this.updateFilter({sepia: e.target.value})}/>
+                        </span>
+                    </div>
                 }
             </div>
         );
